43a8c7c7387b324b41006ac6f2a8de44273e3ad3
bluedelaw <bluedelaw@daniel.law1128@gmail.com>
2024-02-20 11:05:48 -0800

    created 2d array

--- assignment2.c
+++ assignment2.c
@@ -2,7 +2,24 @@
 
 
 
-void simulate(char arr, int x, int y, int t) {
+void simulate(char **arr, int x, int y, int t) {
+
+    int newX = x + 2;
+    int newY = y + 2;
+
+    char **newArr = (char **)malloc(newX * newY * sizeof(char *));
+    
+        for (int i = 0; i < newX; i++) {
+            newArr[i] = (char *)malloc(newY * sizeof(char));
+        }
+
+    for (int i = 0; i < newX; i++) {
+        for (int j = 0; j < newY; j++) {
+            if (i == 0 || i == -1 || j == 0 || j == -1) {
+                newArr[i][j] = '*';
+            }
+        }
+    }
 
 }
 
@@ -16,4 +33,5 @@ int main(int argc, char *argv[])
 
     char *inputFileName = argv[1];
     char *outputFileName = argv[2];
+
 }
