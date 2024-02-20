2a5b23d02e06a7ae158bc6a5b708514ac20c0fd8
Wayne Chen <wchen206@my.bcit.ca>
2024-02-20 14:20:01 -0800

    fixed some typo

--- assignment2.c
+++ assignment2.c
@@ -37,56 +37,57 @@ void updateGrid(char ***arr, int x, int y, Particle *parr, int count)
   }
 
   for (int p = 0; p < count && parr[p].colided != 1; p++) {
-    newArr[parr[p].y + 2][parr[p].x + 2] = '+';
+    newArr[parr[p].y + 3][parr[p].x + 2] = '+';
   }
 
   *arr = newArr;
 }
 
 // call this function in a for loop for each second the simulation runs.
-void moveParticles(Particle *arr, int length, int gridLengthx, int gridLengthy){
-// for each particle in the array given.
-  for(int i = 0; i < length; i++){
-    if(arr[i].colided != 0){
-    // if the particle is going to move in the x direction
-    if(arr[i].vx != 0){
-    // check if the particle will move past the maximum if so bounce, then invert the velocity
-    if(arr[i].x + arr[i].vx > gridLengthx){
-      arr[i].x = gridLengthx - (gridLengthx - (arr[i].vx + arr[i].x));
-      arr[i].vx = arr[i].vx * -1;
-    // if past the minimum, bounce and invert
-    } else if (arr[i].x + arr[i].vx < 0 ){
-            arr[i].x = (arr[i].vx + arr[i].x) * -1;
-            arr[i].vx = arr[i].vx * -1;
-    } else{ //regularly move the particle
-      arr[i].x = arr[i].x + arr[i].vx;
-    }
-    }
-    // does the same thing for y as it did for x.
+void moveParticles(Particle *arr, int length, int gridLengthx, int gridLengthy) {
+    // for each particle in the array given.
+    for (int i = 0; i < length; i++) {
+        if (arr[i].colided == 0) {
+            // if the particle is going to move in the x direction
+            if (arr[i].vx != 0) {
+                // check if the particle will move past the maximum if so bounce, then invert the velocity
+                if (arr[i].x + arr[i].vx > gridLengthx) {
+                    arr[i].x = gridLengthx - (arr[i].x + arr[i].vx - gridLengthx);
+                    arr[i].vx = -arr[i].vx;
+                }
+                // if past the minimum, bounce and invert
+                else if (arr[i].x + arr[i].vx < 0) {
+                    arr[i].x = -(arr[i].x + arr[i].vx);
+                    arr[i].vx = -arr[i].vx;
+                }
+                // regularly move the particle
+                else {
+                    arr[i].x = arr[i].x + arr[i].vx;
+                }
+            }
+            // does the same thing for y as it did for x.
 
-    if(arr[i].vy != 0){
-    if(arr[i].y + arr[i].vy > gridLengthy){
-      arr[i].y = gridLengthy - (gridLengthy - (arr[i].vy + arr[i].y));
-      arr[i].vy = arr[i].vy * -1;
-
-    } else if (arr[i].y + arr[i].vy < 0 ){
-            arr[i].y = (arr[i].vy + arr[i].y) * -1;
-            arr[i].vy = arr[i].vy * -1;
-    } else{
-      arr[i].y = arr[i].y + arr[i].vy;
+            if (arr[i].vy != 0) {
+                if (arr[i].y + arr[i].vy > gridLengthy) {
+                    arr[i].y = gridLengthy - (arr[i].y + arr[i].vy - gridLengthy);
+                    arr[i].vy = -arr[i].vy;
+                } else if (arr[i].y + arr[i].vy < 0) {
+                    arr[i].y = -(arr[i].y + arr[i].vy);
+                    arr[i].vy = -arr[i].vy;
+                } else {
+                    arr[i].y = arr[i].y + arr[i].vy;
+                }
+            }
+            //check if the particles have collided. if they collide, set their velocity to 0, making them stop
+            for (int a = i + 1; a < length; a++) {
+                if (arr[i].x == arr[a].x && arr[i].y == arr[a].y) {
+                    arr[i].colided = 1;
+                    arr[a].colided = 1;
+                    break;
+                }
+            }
+        }
     }
-    }
-    //check if the particles have colided. if they colide, set their velocity to 0, making them stop
-    for(int a = 0; a < length; a++){
-      if(arr[i].x == arr[a].x && arr[i].y == arr[i].y){
-        arr[i].colided = 1;
-        arr[a].colided = 1;
-      }
-    }
-    }
-}
-
-
 }
 
 Particle makeParticle(int x, int y, int vx, int vy){
@@ -135,7 +136,9 @@ int main(int argc, char *argv[])
 
     for (int i = 0; i < particle_count; i++) {
       printf("%d", particles[i].x);
-      printf("%d\n", particles[i].y);
+      printf("%d", particles[i].y);
+      printf("%d", particles[i].vx);
+      printf("%d\n", particles[i].vy);
     }
 
     // Allocate memory for the 2D array
@@ -158,6 +161,10 @@ int main(int argc, char *argv[])
     //the size of the border
     int borderRows = rows + 2;
     int borderCols = cols + 2;
+
+    for (int i = time; i > 0; i--) {
+      moveParticles(particles, particle_count, cols, rows);
+    }
 
     updateGrid(&arr, borderRows, borderCols, particles, particle_count);
 
