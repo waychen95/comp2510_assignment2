bacc857df5a1691afca7f2db1c2c4224d284407a
bluedelaw <bluedelaw@daniel.law1128@gmail.com>
2024-02-20 10:40:03 -0800

    created basic sleketon

--- assignment2.c
+++ assignment2.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+
+
+void simulate(char arr, int x, int y, int t) {
+
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: %s <input_file> <output_file\n", argv[0]);
+        return 1;
+    }
+
+    char *inputFileName = argv[1];
+    char *outputFileName = argv[2];
+}
